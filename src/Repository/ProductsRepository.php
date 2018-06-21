92f37c89cb210f84aa2ce9bea1937922724bd858
greeflas <vldmr.kuprienko@gmail.com>
2018-06-21 21:00:56 +0300

    Null coalesce operator

--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -9,9 +9,24 @@ namespace App\Repository;
 
 class ProductsRepository
 {
+    const DEFAULT_LIMIT = 50;
+
     public function getAll(): array
     {
         return [];
+    }
+
+    public function getProductForSale(array $filter)
+    {
+//        $limit = null;
+//
+//        if (isset($filter['limit'])) {
+//            $limit = $filter['limit'];
+//        }
+
+        $limit = $filter['limit'] ?? self::DEFAULT_LIMIT;
+
+        return $limit;
     }
 
     public function count(array $filters): int
