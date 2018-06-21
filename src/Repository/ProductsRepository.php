24926a2457f3012aa011a77d3726b60fc474d6ec
greeflas <vldmr.kuprienko@gmail.com>
2018-06-21 21:08:08 +0300

    Anonymous class

--- src/Repository/ProductsRepository.php
+++ src/Repository/ProductsRepository.php
@@ -11,6 +11,12 @@ class ProductsRepository
 {
     const DEFAULT_LIMIT = 50;
 
+    private $products = [
+        'PHPStrom',
+        'WebStorm',
+        'Visual Studio',
+    ];
+
     public function getAll(): array
     {
         return [];
@@ -32,5 +38,45 @@ class ProductsRepository
     public function count(array $filters): int
     {
         return '0';
+    }
+
+    public function getProductCollection()
+    {
+        $products = $this->products;
+
+        return new class($products) implements \ArrayAccess {
+
+            private $items;
+
+            public function __construct($items)
+            {
+                $this->items = $items;
+            }
+
+            public function offsetExists($offset)
+            {
+                return isset($this->items[$offset]);
+            }
+
+            public function offsetGet($offset)
+            {
+                return $this->items[$offset];
+            }
+
+            public function offsetSet($offset, $value)
+            {
+                throw $this->createReadOnlyException();
+            }
+
+            public function offsetUnset($offset)
+            {
+                throw $this->createReadOnlyException();
+            }
+
+            private function createReadOnlyException()
+            {
+                return new \LogicException('Products collection is read only');
+            }
+        };
     }
 }
