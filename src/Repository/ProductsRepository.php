059020572f012232500c8fddb766803a4843bd14
greeflas <vldmr.kuprienko@gmail.com>
2018-06-21 20:56:10 +0300

    Type hints, strict type

---
+++ src/Repository/ProductsRepository.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * ITEA PHP 7
+ */
+
+namespace App\Repository;
+
+class ProductsRepository
+{
+    public function getAll(): array
+    {
+        return [];
+    }
+
+    public function count(array $filters): int
+    {
+        return '0';
+    }
+}
